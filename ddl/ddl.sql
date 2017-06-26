134a40f280c0f49607b2d1ad4acf72853a8cd7e8
michaelkunc <mjkunc@gmail.com>
2017-06-26 13:18:21 -0700

    updated ddl and er_diagram

--- ddl/ddl.sql
+++ ddl/ddl.sql
@@ -1,8 +1,9 @@
 --create db 
 drop database if exists electronics;
 create database electronics;
+use electronics;
 
---create order_headers tables
+--order_headers
 drop table if exists order_headers;
 
 create table order_headers (
@@ -15,10 +16,9 @@ create table order_headers (
   po_id varchar(10),
   currency varchar(5),
   primary key (header_id)
-
 );
 
---create order_lines
+--order_lines
 drop table if exists order_lines;
 
 create table order_lines (
@@ -37,7 +37,7 @@ create table order_lines (
   primary key (line_id)
 );
 
---create shipping_types
+--shipping_types
 drop table if exists shipping_types;
 
 create table shipping_types (
@@ -49,7 +49,7 @@ create table shipping_types (
   primary key (shipping_type_id)  
 );
 
---create customer table
+--customers
 drop table if exists customers;
 
 create table customers (
@@ -69,7 +69,7 @@ create table customers (
 );
 
 
--- create state_prov
+--states_provs
 drop table if exists states_provs;
 
 create table states_provs (
@@ -81,7 +81,7 @@ create table states_provs (
   primary key (state_prov_id)  
 );
 
--- create country table
+--countries
 drop table if exists countries;
 
 create table countries (
@@ -92,7 +92,7 @@ create table countries (
   primary key (country_id)  
 );
 
--- create products table
+-- products
 drop table if exists products;
 
 create table products (
@@ -102,27 +102,91 @@ create table products (
   description varchar(2000),
   uom varchar(20) not null,
   manufacturer_id int,
-  family_id int references family(family_id),
-  subfamily_id int references subfamily(subfamily_id),
+  family_id int references product_family(family_id),
+  subfamily_id int references product_subfamily(subfamily_id),
     created_date timestamp default current_timestamp,
   last_updated_date timestamp default current_timestamp,
   primary key (product_id)
 );
 
+--product_subfamily
+drop table if exists product_subfamily;
 
-
---create table subfamily
-drop table if exists subfamily;
-
-create table subfamily (
+create table product_subfamily (
   subfamily_id int not null,
   name varchar(500),
-  family_id int references family(family_id),
+  family_id int references product_family(family_id),
   created_date timestamp default current_timestamp,
   last_updated_date timestamp default current_timestamp,
   primary key (subfamily_id)
 );
   
-  
+--product_family table
+drop table if exists product_family;
+
+create table product_family (
+  family_id int not null,
+  name varchar(500),
+  created_date timestamp default current_timestamp,
+  last_updated_date timestamp default current_timestamp,
+  primary key (family_id)  
+);  
+
+--product costs (will need to make a view so current prices are available)
+drop table if exists product_costs;
+
+create table product_costs (
+  cost_id int not null auto_increment,
+  product_id int references product(product_id),
+  mtl_cost decimal(6, 2),
+  labor_cost decimal(6,2),
+  burden_cost decimal(6,2),
+  created_date timestamp default current_timestamp,
+  primary key (cost_id)
+);
+
+
+--create view to get current_product_cost
+create or replace view current_product_costs as 
+  select pc.product_id, pc.mtl_cost, pc.labor_cost, pc.burden_cost, pc.cost_id, pc.created_date
+  from product_costs pc
+  inner join (
+    select max(cost_id) as cost_id, product_id
+    from product_costs
+    group by 2
+    ) most_recent
+  on pc.cost_id = most_recent.cost_id
+;
+
+drop table if exists products_prices;
+
+create table products_prices (
+    price_list_id int references price_lists(price_list_id),
+    product_id int references products(product_id),
+    list_price decimal(6,2),
+    created_date timestamp default current_timestamp
+);
+
+drop table if exists price_lists;
+
+create table price_lists (
+  price_list_id int not null auto_increment,
+  active boolean,
+  list_start_date date,
+  list_end_date date,
+  created_date timestamp default current_timestamp,
+  last_updated_date timestamp default current_timestamp,
+  primary key (price_list_id)
+);
+
+--create view for current prices 
+create or replace view current_product_prices as
+select pp.product_id, pp.list_price, pl.price_list_id
+from products_prices pp
+inner join price_lists pl
+on pp.price_list_id = pl.price_list_id
+where pl.price_list_id = true
+;
+
 
   
