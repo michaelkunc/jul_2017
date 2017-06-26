036faba8ad3259ee979fd0ce7db5b489ed8b15bd
michaelkunc <mjkunc@gmail.com>
2017-06-26 09:15:51 -0700

    changes to ddl

--- ddl/ddl.sql
+++ ddl/ddl.sql
@@ -49,3 +49,80 @@ create table shipping_types (
   primary key (shipping_type_id)  
 );
 
+--create customer table
+drop table if exists customers;
+
+create table customers (
+  customer_id int not null,
+  name varchar(500),
+  address_1 varchar(100),
+  address_2 varchar(100),
+  city varchar(100),
+  state_prov_id int references state_prov(state_prov_id),
+  country_id int references state_prov(country_id),
+  postal_code varchar(20), 
+  ship_to boolean,
+  sold_to boolean,
+  created_date timestamp default current_timestamp,
+  last_updated_date timestamp default current_timestamp,
+  primary key (customer_id)
+);
+
+
+-- create state_prov
+drop table if exists states_provs;
+
+create table states_provs (
+  state_prov_id int not null,
+  name varchar(500),
+  country_id int references country(country_id),
+  created_date timestamp default current_timestamp,
+  last_updated_date timestamp default current_timestamp,
+  primary key (state_prov_id)  
+);
+
+-- create country table
+drop table if exists countries;
+
+create table countries (
+  country_id int not null,
+  name varchar(500) not null,
+  created_date timestamp default current_timestamp,
+  last_updated_date timestamp default current_timestamp,
+  primary key (country_id)  
+);
+
+-- create products table
+drop table if exists products;
+
+create table products (
+  product_id int not null,
+  product_number varchar(100) not null,
+  name varchar(500),
+  description varchar(2000),
+  uom varchar(20) not null,
+  manufacturer_id int,
+  family_id int references family(family_id),
+  subfamily_id int references subfamily(subfamily_id),
+    created_date timestamp default current_timestamp,
+  last_updated_date timestamp default current_timestamp,
+  primary key (product_id)
+);
+
+
+
+--create table subfamily
+drop table if exists subfamily;
+
+create table subfamily (
+  subfamily_id int not null,
+  name varchar(500),
+  family_id int references family(family_id),
+  created_date timestamp default current_timestamp,
+  last_updated_date timestamp default current_timestamp,
+  primary key (subfamily_id)
+);
+  
+  
+
+  
