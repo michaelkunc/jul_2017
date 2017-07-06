64741eea1440801b0890824f06b5bba69bb88f0c
michaelkunc <mjkunc@gmail.com>
2017-07-06 12:40:51 -0700

    add docstring

--- order_numbers.py
+++ order_numbers.py
@@ -3,6 +3,7 @@ from string import ascii_uppercase, digits
 
 
 def order_number():
+    """Return fake order number in the following format 714-EO1-56237-B"""
     prefix = ''.join(random.sample(digits, 3))
     mid = ''.join(random.sample(ascii_uppercase, 2) + random.sample(digits, 1))
     end = ''.join(random.sample(digits, 5))
