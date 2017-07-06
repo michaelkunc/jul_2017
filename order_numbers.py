fd1474a79f0258dd9dd0d0c396dcfced8bcc836f
michaelkunc <mjkunc@gmail.com>
2017-07-06 15:31:43 -0700

    add function for random id

--- order_numbers.py
+++ order_numbers.py
@@ -9,3 +9,22 @@ def order_number():
     end = ''.join(random.sample(digits, 5))
     post = ''.join(random.sample(ascii_uppercase, 1))
     return '{0}-{1}-{2}-{3}'.format(prefix, mid, end, post)
+
+
+# code for returning a random id for insertion into the customer fields in
+# the order headers table
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.sql.expression import func
+
+engine = create_engine(
+    'mysql+pymysql://michaelkunc:welcome123@localhost/demo')
+
+Session = sessionmaker(bind=engine)
+session = Session()
+
+
+def get_random_id(table):
+    """Takes a SQLAlchemy class name as a parameter."""
+    return session.query(table).order_by(func.rand()).first().id
