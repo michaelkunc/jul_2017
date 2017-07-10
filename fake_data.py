cc706ef8b2e48dbd5ad0c03a4754284809b1ffd0
michaelkunc <mjkunc@gmail.com>
2017-07-10 15:57:22 -0700

    product fake data complete

---
+++ fake_data.py
@@ -0,0 +1,53 @@
+import random
+from string import ascii_uppercase, digits
+
+
+class Product(object):
+
+    FAMILIES = {1: 'Electronic Kits & Modules', 2: 'Test & Measurment', 3: 'Tools & Equipment',
+                4: 'Components & Hardware'}
+
+    SUBFAMILIES = ['Misc Electronic Components', 'Component Packs', 'Pre-Programmed Firmware',
+                   'Controller Automation', 'Audio Kits', 'Sensing Device', 'Relay Board Kits',
+                   'Data Loggers', 'Soldering', 'Power Inverters', 'Microcontrollers', 'Electrical Hardware',
+                   'Security Kits']
+
+    def __init__(self):
+        pass
+
+    def part_number(self):
+        prefix = ''.join(random.sample(ascii_uppercase, 3))
+        suffix = ''.join(random.sample(digits, 8))
+        return '{0}-{1}'.format(prefix, suffix)
+
+    def name(self):
+        nouns = ('Resistor', 'Potentiometer', 'Capacitor',
+                 'Inductor', 'Oscillator', 'Relay', 'Transformer', 'Battery', 'Integrated Circuit',
+                 'Display', 'Condenser', 'Reactor', 'Isolator', 'Control Knob', 'PWB', 'Diode',
+                 'Thermistor', 'CMOS', 'Timer', 'Comparator', 'Regulator', 'Aplifier', 'Cerebra', 'Cerebro')
+
+        adjectives = ('Active', 'Arc', 'DC', 'Fused', 'Passive',
+                      'Electromechanical', 'Constant Current', 'MOSFET', 'Incandescent',
+                      'Diode', 'MIS', 'Piezoelectrical', 'Choke', 'Solenoid', 'Selenium',
+                      'Distributed', 'Voltage Regulation', 'Light Emitting', 'Variable Capacitance',
+                      'Carbon Film', 'Metal Film', 'Variable', 'CDS', 'NTC', 'PTC', 'CTR', 'Electrolytic',
+                      'Tantalum', 'Ceramic', 'Multilayer', 'Polystyrene', 'Polypropylene', 'Mica', 'Repulsing')
+
+        return '{0} {1}'.format(random.choice(adjectives), random.choice(nouns))
+
+    def description(self):
+        descriptions = ('FLIP-FLOP, 2 CIRCUITS', 'Logic IC Case Style',
+                        'PDIP', 'No. of Pins: 14', 'Case Style: PDIP', 'Single Transmitter/Receiver',
+                        'RS-422/RS-485', '8-Pin PDIP Tube', 'XOR Gate', '4-Element', '2-IN Bipolar',
+                        '14-Pin PDIP,XOR Gate', '4-Element 2-IN', 'Bipolar 14-Pin', 'PDIP XOR Gate',
+                        'IBUS', 'JIS', 'DC Block Type', 'Electrical Coil Sensor', 'Type 76553', 'Fiber Optic Circuit',
+                        'Constant Input Resistance', 'Constant Output resistance', 'TI', 'Stark Industries Model',
+                        'Reed Richards Design', 'Later Design Type', 'Reference 22320f', 'Von Doom Captive Design',
+                        'McCoy Style Conversion', 'Cho Model', 'Log Counter Implementatoon', 'MK VI', 'MKVII',
+                        'MacTaggert Implementation', 'Pym Particle Reduction', 'Bannertech')
+
+        return '{0} {1}'.format(random.choice(descriptions), random.choice(descriptions))
+
+    def uom(self):
+        uoms = ('Each', 'Case', '12 Pack', 'Pallet', '24 Pack')
+        return random.choice(uoms)
--- product_table.py
+++ product_table.py
@@ -1,19 +1,18 @@
-import requests
-import os
 import pymysql
 from faker import Faker
-from sqlalchemy import Column, String, Integer, ForeignKey
+from sqlalchemy import Column, String, Integer, ForeignKey, Numeric
 from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
 Base = declarative_base()
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 import random
+import fake_data
+from sqlalchemy.sql.expression import func
+import time
 
-# note - API key stored as an environment variable.
 
-
-# SQL Alchemy Stuff
+# Table Def
 class Product_Family(Base):
     __tablename__ = 'product_family'
     family_id = Column(Integer, primary_key=True)
@@ -41,60 +40,49 @@ class Products(Base):
     subfamily = relationship(Product_Subfamily, foreign_keys=[subfamily_id])
     family = relationship(Product_Subfamily, foreign_keys=family_id)
 
-# connect to API and bring back some data
+
+class ProductCosts(Base):
+    __tablename__ = 'product_costs'
+    cost_id = Column(Integer, primary_key=True)
+    product_id = Column(Integer)
+    mtl_cost = Column(Numeric)
+    labor_cost = Column(Numeric)
+    burden_cost = Column(Numeric)
+
+# Hydrate the tables
 engine = create_engine(
     'mysql+pymysql://michaelkunc:welcome123@localhost/electronics')
 
 Session = sessionmaker(bind=engine)
 session = Session()
 
-# url = 'https://octopart.com/api/v3/parts/match?apikey={}&queries=[{{"mpn":"SN7*", "limit":20}}]&pretty_print=true&include[]=short_description&include[]=descriptions'.format(
-#     os.environ['API_KEY'])
-# r = requests.get(url)
-# data = r.json()
-
-
-# for item in data['results'][0]['items']:
-#     product = Products(product_number=item['mpn'],
-#                        name=item['brand']['name'],
-#                        description=item['short_description'],
-#                        uom='Each')
-#     session.add(product)
-#     session.commit()
-#     session.close()
-
-import fake_data
-from sqlalchemy.sql.expression import func
-
 
 def populate_families():
-    for k, v in fake_data.ProductFamily.items():
+    for k, v in fake_data.Product.FAMILIES.items():
         data = Product_Family(family_id=k,
                               name=v)
         session.add(data)
         session.commit()
     session.close()
+    print('product_family table has been populated.')
 
 
 def populate_subfamilies():
-    for i in fake_data.ProductSubFamily:
+    for i in fake_data.Product.SUBFAMILIES:
         data = Product_Subfamily(name=i, family_id=random.choice(
-            list(fake_data.ProductFamily.keys())))
+            list(fake_data.Product.FAMILIES.keys())))
         session.add(data)
         session.commit()
     session.close()
-
-
-def get_subfamily_id():
-    """Takes a SQLAlchemy class name as a parameter."""
-    row = session.query(Product_Subfamily).order_by(func.rand()).first()
-    return (row.subfamily_id, row.family_id)
+    print('product_subfamily table has been populated.')
 
 
 def populate_products(number):
+    print('Populating products table.')
     for i in range(0, number):
         fake = fake_data.Product()
-        subfamily = get_subfamily_id()
+        row = session.query(Product_Subfamily).order_by(func.rand()).first()
+        subfamily = (row.subfamily_id, row.family_id)
         data = Products(product_number=fake.part_number(),
                         name=fake.name(),
                         description=fake.description(),
@@ -103,9 +91,31 @@ def populate_products(number):
                         family_id=subfamily[1])
         session.add(data)
         session.commit()
-
     session.close()
+    print('products table has been populated.')
+
+
+def populate_prices():
+    for i in session.query(Products.product_id):
+        data = ProductCosts(product_id=i[0],
+                            mtl_cost=round(random.uniform(.1, 10), 2),
+                            labor_cost=round(
+                                random.uniform(.1, 10), 2),
+                            burden_cost=round(
+                                random.uniform(.1, 10), 2)
+                            )
+        session.add(data)
+        session.commit()
+    session.close()
+    print('product_costs table has been populated.')
+
 
 populate_families()
 populate_subfamilies()
 populate_products(2000)
+# calling prices 3 times to generate some historical data
+populate_prices()
+time.sleep(5)
+populate_prices()
+time.sleep(5)
+populate_prices()
